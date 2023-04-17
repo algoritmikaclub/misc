744e789128ce0d0664ee52bfbd8bf5774751d730
SergeyKalutsky <skalutsky@gmail.com>
2023-04-17 07:35:31 +0300

    add apis

---
+++ Python/telegram_bot/apis.py
@@ -0,0 +1,18 @@
+import requests
+from credentials import GPT_TOKEN
+
+
+def get_random_duck():
+    endpoint = 'https://random-d.uk/api/random'
+    response = requests.get(endpoint)
+    data = response.json()
+    return data['url']
+
+
+def ask_chat_gpt(question):
+    url = 'https://api.openai.com/v1/chat/completions'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {GPT_TOKEN}'}
+    data = {'model': 'gpt-3.5-turbo','messages': [{'role': 'user', 'content': question}]}
+    response = requests.post(url, headers=headers, json=data)
+    data = response.json()
+    return data['choices'][0]['message']['content']
--- Python/telegram_bot/main.py
+++ Python/telegram_bot/main.py
@@ -1,7 +1,9 @@
 from telebot import TeleBot
+from credentials import TOKEN
+from apis import get_random_duck, ask_chat_gpt
 
-TOKEN = '6275999824:AAHN4qstpUwsOO1Rk2EQ0K3jeyTXqITd18k'
 bot = TeleBot(TOKEN)
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -9,10 +11,22 @@ def start(message):
     bot.send_message(message.chat.id, text=answer, parse_mode='html')
 
 
+@bot.message_handler(commands=['duck'])
+def duck(message):
+    url = get_random_duck()
+    bot.send_message(message.chat.id, text=url)
+
+
+@bot.message_handler(commands=['GPT'])
+def duck(message):
+    bot.send_message(message.chat.id, text='Подождите, генерирую ответ...')
+    answer = ask_chat_gpt(message.text[4:])
+    bot.send_message(message.chat.id, text=answer)
+
+
 @bot.message_handler()
 def on_message(message):
     bot.send_message(message.chat.id, text=message.text)
-
 
 
 if __name__ == '__main__':
